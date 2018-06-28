8db69c3a975b89cb2deea7ac52782389b9619c4b
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-06-28 14:47:33 -0400

    Update Iterating-over-iterables-(1).py

--- Python Data Science Toolbox -Part 2/Iterating-over-iterables-(1).py
+++ Python Data Science Toolbox -Part 2/Iterating-over-iterables-(1).py
@@ -8,8 +8,6 @@ INSTRUCTIONS
 Create a for loop to loop over flash and print the values in the list. Use person as the loop variable.
 Create an iterator for the list flash and assign the result to superspeed.
 Print each of the items from superspeed using next() 4 times.
-
-SCRIPT.PY
 '''
 
 # Create a list of strings: flash
