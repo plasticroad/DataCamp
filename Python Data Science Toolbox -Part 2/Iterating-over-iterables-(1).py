c42782b3302fbf32fc366405d8f0744698212c4b
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-06-28 14:47:00 -0400

    Update and rename Iterating-over-iterables-(1) to Iterating-over-iterables-(1).py

--- Python Data Science Toolbox -Part 2/Iterating-over-iterables-(1).py
+++ Python Data Science Toolbox -Part 2/Iterating-over-iterables-(1).py
@@ -1,3 +1,4 @@
+'''
 Great, you're familiar with what iterables and iterators are! In this exercise, you will reinforce your knowledge about these by iterating over and printing from iterables and iterators.
 
 You are provided with a list of strings flash. You will practice iterating over the list by using a for loop. You will also create an iterator for the list and access the values from the iterator.
@@ -9,6 +10,7 @@ Create an iterator for the list flash and assign the result to superspeed.
 Print each of the items from superspeed using next() 4 times.
 
 SCRIPT.PY
+'''
 
 # Create a list of strings: flash
 flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
