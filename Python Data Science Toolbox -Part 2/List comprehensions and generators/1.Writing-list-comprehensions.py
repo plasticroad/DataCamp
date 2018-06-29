f73483c032975a092251a16e851e4636d8068ec7
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-06-28 20:27:53 -0400

    Create 1.Writing-list-comprehensions.py

---
+++ Python Data Science Toolbox -Part 2/List comprehensions and generators/1.Writing-list-comprehensions.py
@@ -0,0 +1,11 @@
+'''
+Writing list comprehensions
+You now have all the knowledge necessary to begin writing list comprehensions! Your job in this exercise is to write a list comprehension that produces a list of the squares of the numbers ranging from 0 to 9.
+
+INSTRUCTIONS
+100 XP
+Using the range of numbers from 0 to 9 as your iterable and i as your iterator variable, write a list comprehension that produces a list of numbers consisting of the squared values of i.
+'''
+
+# Create list comprehension: squares
+squares = [____ for ____ in ____]
