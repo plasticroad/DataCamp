fd1efdbd76d5d87baeec65539ed2c0a90212f5fa
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-06-29 13:30:05 -0400

    Update 3.Using-a-list-comprehension.py

--- Python Data Science Toolbox -Part 2/3.Bringing it all together/3.Using-a-list-comprehension.py
+++ Python Data Science Toolbox -Part 2/3.Bringing it all together/3.Using-a-list-comprehension.py
@@ -13,3 +13,16 @@ Create a list comprehension that generates a dictionary using lists2dict() for e
 Look at the first two dictionaries in list_of_dicts by printing them out.
 '''
 
+print(feature_names)
+print(row_lists)
+
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names,sublist) for sublist in row_lists]
+
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
