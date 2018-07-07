2b5ba24eda00ea40165835432eba5f1e52dfc106
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-07-06 22:13:23 -0400

    Create 03.The-Hello-World-of-SQL-Queries!.py

---
+++ Python/05.Importing-Data-in-Python--Part-1/03.Working-with-relational-databases-in-Python/03.The-Hello-World-of-SQL-Queries!.py
@@ -0,0 +1,17 @@
+'''
+The Hello World of SQL Queries!
+
+Now, it's time for liftoff! In this exercise, you'll perform the Hello World of 
+SQL queries, SELECT, in order to retrieve all columns of the table Album in the 
+Chinook database. Recall that the query SELECT * selects all columns.
+
+INSTRUCTIONS
+100 XP
+Open the engine connection as con using the method connect() on the engine.
+Execute the query that selects ALL columns from the Album table. Store the results 
+in rs.
+Store all of your query results in the DataFrame df by applying the fetchall() 
+method to the results rs.
+Close the connection!
+'''
+
