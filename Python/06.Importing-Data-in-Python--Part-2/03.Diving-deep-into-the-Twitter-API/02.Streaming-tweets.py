f0535fdf3462fa3ec8e5c3d6aa080eeac3759f1b
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-07-11 19:45:13 -0400

    Update 02.Streaming-tweets.py

--- Python/06.Importing-Data-in-Python--Part-2/03.Diving-deep-into-the-Twitter-API/02.Streaming-tweets.py
+++ Python/06.Importing-Data-in-Python--Part-2/03.Diving-deep-into-the-Twitter-API/02.Streaming-tweets.py
@@ -17,3 +17,12 @@ To filter Twitter streams, pass to the track argument in stream.filter() a list
 containing the desired keywords 'clinton', 'trump', 'sanders', and 'cruz'.
 '''
 
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton','trump','sanders','cruz'])
