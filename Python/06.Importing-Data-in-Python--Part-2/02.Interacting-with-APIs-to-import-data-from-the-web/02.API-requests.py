b4b365f6346c2631e389043dabeeec00d3bcebe2
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-07-11 15:11:30 -0400

    Update 02.API-requests.py

--- Python/06.Importing-Data-in-Python--Part-2/02.Interacting-with-APIs-to-import-data-from-the-web/02.API-requests.py
+++ Python/06.Importing-Data-in-Python--Part-2/02.Interacting-with-APIs-to-import-data-from-the-web/02.API-requests.py
@@ -24,3 +24,14 @@ Print the text of the reponse object r by using its text attribute and passing t
 result to the print() function.
 '''
 
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
