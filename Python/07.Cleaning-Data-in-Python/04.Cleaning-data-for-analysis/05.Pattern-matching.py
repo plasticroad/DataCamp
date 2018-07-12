fe053c25266efb463596e3c0c628bbbf57821657
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-07-12 15:29:20 -0400

    Create 05.Pattern-matching.py

---
+++ Python/07.Cleaning-Data-in-Python/04.Cleaning-data-for-analysis/05.Pattern-matching.py
@@ -0,0 +1,20 @@
+'''
+Pattern matching
+
+In this exercise, you'll continue practicing your regular expression skills. 
+For each provided string, your job is to write the appropriate pattern to match it.
+
+INSTRUCTIONS
+100 XP
+Write patterns to match:
+A telephone number of the format xxx-xxx-xxxx. You already did this in a previous 
+exercise.
+A string of the format: A dollar sign, an arbitrary number of digits, a decimal 
+point, 2 digits.
+Use \$ to match the dollar sign, \d* to match an arbitrary number of digits, \. to 
+match the decimal point, and \d{x} to match x number of digits.
+A capital letter, followed by an arbitrary number of alphanumeric characters.
+Use [A-Z] to match any capital letter followed by \w* to match an arbitrary number 
+of alphanumeric characters.
+'''
+
