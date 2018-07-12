97858b16dc08f49cb3c0fd64ef1697a9a3c43aba
Ricardo Contreras Corona <30706902+plasticroad@users.noreply.github.com>
2018-07-11 20:41:11 -0400

    Update 06.Plotting-your-Twitter-data.py

--- Python/06.Importing-Data-in-Python--Part-2/03.Diving-deep-into-the-Twitter-API/06.Plotting-your-Twitter-data.py
+++ Python/06.Importing-Data-in-Python--Part-2/03.Diving-deep-into-the-Twitter-API/06.Plotting-your-Twitter-data.py
@@ -22,3 +22,17 @@ appear on the x-axis; the second argument should be the list of the variables yo
 wish to plot, as produced in the previous exercise.
 '''
 
+# Import packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(['clinton', 'trump', 'sanders', 'cruz'], [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
